ebca80898c23d98c616217212f3e4e4309e8ff4c
dbenitez-bcn <dbr.bcn@gmail.com>
2021-11-25 22:21:35 +0100

    upadtes UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,72 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sharlyapp/data/datasources/firebase_users_repository.dart';
+import 'package:sharlyapp/presentation/blocs/user/user_bloc.dart';
 import 'package:sharlyapp/presentation/pages/main_page.dart';
+import 'package:sharlyapp/services/auth_service.dart';
 
 void main() {
   runApp(Sharly());
 }
 
 class Sharly extends StatelessWidget {
+  Widget _message(String message) {
+    return Scaffold(
+      body: Center(
+        child: Text(message),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: FutureBuilder(
-          future: Firebase.initializeApp(),
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return const Center(
-                child: Text("No pudimos iniciar la Sharly..."),
-              );
-            }
-
-            if (snapshot.connectionState == ConnectionState.done) {
-              return MainPage();
-            }
-
-            return const Center(
-              child: CircularProgressIndicator.adaptive(),
-            );
-          },
-        ));
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: _initializeApp(
+        child: MainPage(),
+      ),
+    );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+  Widget _initializeApp({required Widget child}) {
+    return FutureBuilder(
+      future: Firebase.initializeApp(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return _message("No pudimos iniciar la Sharly...");
+        } else if (snapshot.connectionState == ConnectionState.done) {
+          return _initializeBloc(child);
+        }
+
+        return const Center(
+          child: CircularProgressIndicator.adaptive(),
+        );
+      },
+    );
+  }
+
+  Widget _initializeBloc(Widget child) {
+    return BlocProvider(
+      create: (_) =>
+          UserBloc(AuthService(FirebaseUsersRepository(FirebaseAuth.instance))),
+      child: BlocBuilder<UserBloc, UserState>(
+        builder: (context, state) {
+          if (state is UserAuthSuccess) {
+            return child;
+          } else if (state is UserAuthInProgress) {
+            return const CircularProgressIndicator.adaptive();
+          } else if (state is UserInitial) {
+            context.read<UserBloc>().add(UserSignedIn());
+            return const SizedBox(width: 10, height: 10);
+          } else {
+            return _message("Es tu problemo");
+          }
+        },
       ),
     );
   }
--- lib/presentation/pages/main_page.dart
+++ lib/presentation/pages/main_page.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatelessWidget {
@@ -6,26 +5,10 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Sharly"),
+        title: const Text("Sharly"),
       ),
-      body: FutureBuilder<UserCredential>(
-        future: FirebaseAuth.instance.signInAnonymously(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-          return const Center(
-            child: Text("No pudimos iniciar la Sharly..."),
-          );
-        }
-
-        if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
-          return Center(
-            child: Text(snapshot.data!.user!.uid),
-          );
-        }
-
-
-        return CircularProgressIndicator.adaptive();
-        },
+      body: const Center(
+        child: Text("Hello sharly!"),
       ),
     );
   }
