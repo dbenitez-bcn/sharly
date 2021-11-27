97f1f9804402b28afd59eb83e16f02cf7704e86e
dbenitez-bcn <dbr.bcn@gmail.com>
2021-11-27 21:46:14 +0100

    re organise app

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:sharlyapp/data/datasources/firebase_users_repository.dart';
-import 'package:sharlyapp/presentation/blocs/user/user_bloc.dart';
-import 'package:sharlyapp/presentation/pages/main_page.dart';
-import 'package:sharlyapp/services/auth_service.dart';
 
 void main() {
   runApp(Sharly());
 }
 
 class Sharly extends StatelessWidget {
+  Future<void> _initializeApp() async {
+    await Firebase.initializeApp();
+    await _initSession(ifNew: _createDefaultList);
+  }
+
+  Future<void> _initSession({required Function(String uid) ifNew}) async {
+    if (FirebaseAuth.instance.currentUser == null) {
+      UserCredential userCredentials = await FirebaseAuth.instance.signInAnonymously();
+      if (userCredentials.additionalUserInfo!.isNewUser) await ifNew(userCredentials.user!.uid);
+    }
+  }
+  
+  Future<void> _createDefaultList(String uid) async {
+    DocumentReference defaultList = await FirebaseFirestore.instance.collection("lists").add({
+      "title": "Lista de la compra",
+      "products": []
+    });
+    await FirebaseFirestore.instance.collection("shared_lists").doc(uid).set({
+      "lists": [defaultList.id]
+    });
+  }
+  
   Widget _message(String message) {
     return Scaffold(
       body: Center(
@@ -27,47 +45,59 @@ class Sharly extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
-      home: _initializeApp(
-        child: MainPage(),
-      ),
+      home: FutureBuilder<void>(
+          future: _initializeApp(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              // TODO: Add splash screen to load
+              return const Scaffold(
+                body: Center(child: CircularProgressIndicator.adaptive()),
+              );
+            } else if (snapshot.hasError ||
+                FirebaseAuth.instance.currentUser == null) {
+              return _message("No pudimos iniciar la Sharly...");
+            } else {
+              return _message(FirebaseAuth.instance.currentUser!.uid);
+            }
+          }),
     );
   }
 
-  Widget _initializeApp({required Widget child}) {
-    return FutureBuilder(
-      future: Firebase.initializeApp(),
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return _message("No pudimos iniciar la Sharly...");
-        } else if (snapshot.connectionState == ConnectionState.done) {
-          return _initializeBloc(child);
-        }
-
-        return const Center(
-          child: CircularProgressIndicator.adaptive(),
-        );
-      },
-    );
-  }
-
-  Widget _initializeBloc(Widget child) {
-    return BlocProvider(
-      create: (_) =>
-          UserBloc(AuthService(FirebaseUsersRepository(FirebaseAuth.instance))),
-      child: BlocBuilder<UserBloc, UserState>(
-        builder: (context, state) {
-          if (state is UserAuthSuccess) {
-            return child;
-          } else if (state is UserAuthInProgress) {
-            return const CircularProgressIndicator.adaptive();
-          } else if (state is UserInitial) {
-            context.read<UserBloc>().add(UserSignedIn());
-            return const SizedBox(width: 10, height: 10);
-          } else {
-            return _message("Es tu problemo");
-          }
-        },
-      ),
-    );
-  }
+// Widget _initializeApp({required Widget child}) {
+//   return FutureBuilder(
+//     future: Firebase.initializeApp(),
+//     builder: (context, snapshot) {
+//       if (snapshot.hasError) {
+//         return _message("No pudimos iniciar la Sharly...");
+//       } else if (snapshot.connectionState == ConnectionState.done) {
+//         return _initializeBloc(child);
+//       }
+//
+//       return const Center(
+//         child: CircularProgressIndicator.adaptive(),
+//       );
+//     },
+//   );
+// }
+//
+// Widget _initializeBloc(Widget child) {
+//   return BlocProvider(
+//     create: (_) =>
+//         UserBloc(AuthService(FirebaseUsersRepository(FirebaseAuth.instance))),
+//     child: BlocBuilder<UserBloc, UserState>(
+//       builder: (context, state) {
+//         if (state is UserAuthSuccess) {
+//           return child;
+//         } else if (state is UserAuthInProgress) {
+//           return const CircularProgressIndicator.adaptive();
+//         } else if (state is UserInitial) {
+//           context.read<UserBloc>().add(UserSignedIn());
+//           return const SizedBox(width: 10, height: 10);
+//         } else {
+//           return _message("Es tu problemo");
+//         }
+//       },
+//     ),
+//   );
+// }
 }
