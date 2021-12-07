cfa2ac44b668002e23368d20cc68889f7ab1c797
dbenitez-bcn <dbr.bcn@gmail.com>
2021-12-07 14:14:34 +0100

    adds ad banner

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:sharlyapp/domain/valueObjects/shared_list.dart';
 import 'package:sharlyapp/presentation/blocs/list/list_bloc.dart';
 import 'package:sharlyapp/presentation/pages/main_page.dart';
@@ -49,6 +50,7 @@ class Sharly extends StatelessWidget {
   Future<void> _initializeApp() async {
     await Firebase.initializeApp();
     await _initSession(ifNew: _createDefaultList);
+    await MobileAds.instance.initialize();
   }
 
   Future<void> _initSession({required Function(String uid) ifNew}) async {
--- lib/presentation/pages/main_page.dart
+++ lib/presentation/pages/main_page.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:sharlyapp/presentation/widgets/sharly_list_view.dart';
+import 'package:sharlyapp/utils/ad_helper.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  late BannerAd _bottomBannerAd;
+  bool _isBottomBannerAdLoaded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +25,53 @@ class MainPage extends StatelessWidget {
         onPressed: () => Navigator.pushNamed(context, '/newProduct'),
         child: const Icon(Icons.add),
       ),
+      bottomNavigationBar: SafeArea(
+        child: SizedBox(
+          height: _bottomBannerAd.size.height.toDouble(),
+          width: _bottomBannerAd.size.width.toDouble(),
+          child: _isBottomBannerAdLoaded ? AdWidget(ad: _bottomBannerAd) : null,
+        ),
+      ),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _createBottomBannerAd();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _bottomBannerAd.dispose();
+  }
+
+  void _createBottomBannerAd() {
+    _bottomBannerAd = BannerAd(
+      adUnitId: AdHelper.bannerAdUnitId,
+      size: AdSize.banner,
+      request: const AdRequest(
+        keywords: [
+          "mercadona",
+          "carrefour",
+          "dia",
+          "lidl",
+          "food",
+          "groceries"
+        ],
+      ),
+      listener: BannerAdListener(
+        onAdLoaded: (_) {
+          setState(() {
+            _isBottomBannerAdLoaded = true;
+          });
+        },
+        onAdFailedToLoad: (ad, error) {
+          ad.dispose();
+        },
+      ),
+    );
+    _bottomBannerAd.load();
   }
 }
