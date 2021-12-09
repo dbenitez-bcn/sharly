42026d13dc5e4a05b46fd7979e4fd12aa684ac60
dbenitez-bcn <dbr.bcn@gmail.com>
2021-12-09 13:40:48 +0100

    adds dev environment

---
+++ lib/main_dev.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:sharlyapp/sharly.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  String host = Platform.isAndroid ? '10.0.2.2' : '127.0.0.1';
+  FirebaseFirestore.instance.settings = Settings(
+    host: "$host:8080",
+    sslEnabled: false,
+    persistenceEnabled: false,
+  );
+  await FirebaseAuth.instance.useAuthEmulator(host, 9099);
+  runApp(const Sharly());
+}
